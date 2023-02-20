0ca56bea0f33ab85ab7d9e18eb9f7b5189010d43
Cap <3051927272@qq.com>
2023-02-20 09:41:43 +0800

    界面基本完成，引入js库

--- js/index.js
+++ js/index.js
@@ -1,14 +1,1 @@
-import axios from 'axios'
-
-let button = document.querySelector('button');
-button.addEventListener('click', () => {
-    abc();
-})
-
-function abc() {
-    const res = axios({
-        method: 'GET',
-        url: 'http://www.liulongbin.top:3006/api/getbooks'
-    })
-    console.log(res);
-}
+let 
